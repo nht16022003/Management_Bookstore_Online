4831f68a30e790a081b104682c729fd0631dd06c
Nguyen Hoang Tuan <tuannguyen16022003@gmail.com>
2026-08-16 02:23:25 +0700

    Add UI LoginPage, edit selector in login-page.component.ts is app-login-page, change bootstrap in app.modules.ts is selector in login-page.component.ts, change tags index app-root -> selector in login-page component

--- Frontend/src/app/UI/Page/login_page/login-page.component.ts
+++ Frontend/src/app/UI/Page/login_page/login-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './UI/Page/login_page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginPageComponent]
 })
 export class AppModule { }
